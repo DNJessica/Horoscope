40c6c0c274b99d03ceb3525e3848a47f31036855
DNJessica <dzhesikanimets@gmail.com>
2023-03-31 22:49:59 +0300

    layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body class="text-dark" style="background: linear-gradient(white, #e0b3ff) fixed; height:100%">
+    <nav style="background-color: #a31aff;" class="navbar navbar-expand-md navbar-dark fixed-top">
+        <div class="container-fluid">
+        <img src="https://cdn-icons-png.flaticon.com/32/8062/8062855.png" alt="Horoscope">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            </ul>
+            <form class="d-flex" role="search">
+            <button class="btn btn-outline-light" type="submit">Log in</button>
+            </form>
+        </div>
+        </div>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
